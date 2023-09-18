22f89e97efdae838ceccc66a8a4af07f59a15ddd
Colin Huber <cbhuber@gmail.com>
2023-09-18 14:30:38 -0600

    feat: notes.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Some content</p>
+      <p>Click the notes in the Navbar to see a collection of notes!</p>
     </div>
   );
 }
