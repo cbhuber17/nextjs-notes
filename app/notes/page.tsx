3ad85c87585184292fa74e25d6b4f4e1e6f00da4
Colin Huber <cbhuber@gmail.com>
2023-10-11 16:12:31 -0600

    wip: delete button functionality.

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,26 +2,35 @@ import Link from "next/link";
 import styles from "./Notes.module.css";
 import CreateNote from "./CreateNote";
 import PocketBase from "pocketbase";
+import DeleteButton from "../_components/DeleteButton";
 
 export const dynamic = "force-dynamic";
 
+// DB Details
+const url = "https://next-notes.pockethost.io";
+const db = new PocketBase(url);
+
 async function getNotes() {
-  const url = "https://next-notes.pockethost.io";
-  const db = new PocketBase(url);
-  const res = await db.collection("notes").getFullList({
-    sort: "-created",
-  });
-  return res;
+  try {
+    const res = await db.collection("notes").getFullList({
+      sort: "-created",
+    });
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default async function NotesPage() {
   const notes = await getNotes();
 
+  if (!notes) return;
+
   return (
     <div>
       <h1>Notes</h1>
       <div className={styles.grid}>
-        {notes.map((note) => (
+        {notes!.map((note) => (
           <Note key={note.id} note={note}></Note>
         ))}
       </div>
@@ -35,12 +44,23 @@ function Note({ note }: any) {
   const { id, title, content, created } = note || {};
 
   return (
-    <Link href={`/notes/${id}`}>
-      <div className={styles.note}>
+    <div className={styles.note}>
+      {/* <DeleteButton onClick={() => handleDelete(id)} /> */}
+      <DeleteButton />
+      <Link href={`/notes/${id}`}>
         <h2>{title}</h2>
         <h5>{content}</h5>
         <p>{created}</p>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
+}
+
+async function handleDelete(id: string) {
+  try {
+    await db.collection("notes").delete(id);
+    console.log(`Deleted ${id}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
